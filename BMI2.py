ca28a5cbb433a5b1f80da9f07e2f12c4fa44d536
Paula Szalata <M0110054@mahle.com>
2022-02-28 19:27:15 +0100

    x

--- BMI2.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf8 -*-
-print("Podaje wagę w kg")
-weight = float(input())
-print("Podaj wzrost w m")
-height = float(input())
-BMI = weight / (height ** 2)
-print("Twoje BMI wynosi:", BMI)
-
-      
